35c67f9647607e1092bae3338206d3b4fdd6979f
goll <joaog.bastos@protonmail.ch>
2026-02-22 08:45:41 -0300

    Make formatting and indentation more consistent

--- src/main.ts
+++ src/main.ts
@@ -6,116 +6,116 @@ const previousOpenFile: Map<number, string> = new Map();
 const openTabs: Map<string, number[]> = new Map();
 
 const notifyServer = browser.runtime.connectNative(
-	"tab_reload_notify_server",
+    "tab_reload_notify_server",
 ) as NotifyServerPort;
 
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-	if (!changeInfo.url) {
-		return;
-	}
+    if (!changeInfo.url) {
+        return;
+    }
 
-	// XXX: handle `jar:file://!` URLs (they can have trailing slashes [before the !] too)
-	const fileUriPrefix = "file:///";
-	const previous = previousOpenFile.get(id);
+    // XXX: handle `jar:file://!` URLs (they can have trailing slashes [before the !] too)
+    const fileUriPrefix = "file:///";
+    const previous = previousOpenFile.get(id);
 
-	let file: string;
+    let file: string;
 
-	// To make the code simpler, we will add the relevant entries in the maps
-	// even if the commands sent to the inotify server end up erroring out,
-	// rolling back the changes when we receive an error response.
-	if (tab.url?.startsWith(fileUriPrefix)) {
-		// NOTE: the browser already canonicalizes `..` and `.` but we still have
-		// to canonicalize repeated and trailing slashes
-		file = `/${tab.url.substring(fileUriPrefix.length)}`
-			.replaceAll(/\/+/, "/")
-			.replace(/\/$/, "");
+    // To make the code simpler, we will add the relevant entries in the maps
+    // even if the commands sent to the inotify server end up erroring out,
+    // rolling back the changes when we receive an error response.
+    if (tab.url?.startsWith(fileUriPrefix)) {
+        // NOTE: the browser already canonicalizes `..` and `.` but we still have
+        // to canonicalize repeated and trailing slashes
+        file = `/${tab.url.substring(fileUriPrefix.length)}`
+            .replaceAll(/\/+/, "/")
+            .replace(/\/$/, "");
 
-		if (!openTabs.has(file)) {
-			console.log(`Requesting to watch \`${file}'...`);
+        if (!openTabs.has(file)) {
+            console.log(`Requesting to watch \`${file}'...`);
 
-			notifyServer.postMessage({
-				command: "add",
-				file,
-			});
+            notifyServer.postMessage({
+                command: "add",
+                file,
+            });
 
-			openTabs.set(file, [id]);
-		} else {
-			openTabs.get(file).push(id);
-		}
+            openTabs.set(file, [id]);
+        } else {
+            openTabs.get(file).push(id);
+        }
 
-		previousOpenFile.set(id, file);
-	} else {
-		previousOpenFile.delete(id);
-	}
+        previousOpenFile.set(id, file);
+    } else {
+        previousOpenFile.delete(id);
+    }
 
-	if (previous) {
-		// List of all tabs that are currently open on the
-		// same file that was previously opened on this tab
-		//
-		// At this point, the list should also include this tab, so we have to remove it
-		const tabs = openTabs.get(previous);
-		const tabIndex = tabs.indexOf(id);
+    if (previous) {
+        // List of all tabs that are currently open on the
+        // same file that was previously opened on this tab
+        //
+        // At this point, the list should also include this tab, so we have to remove it
+        const tabs = openTabs.get(previous);
+        const tabIndex = tabs.indexOf(id);
 
-		if (tabIndex === -1) {
-			throw new Error();
-		}
+        if (tabIndex === -1) {
+            throw new Error();
+        }
 
-		if (previous === file) {
-			return;
-		}
+        if (previous === file) {
+            return;
+        }
 
-		if (tabIndex === 0) {
-			openTabs.delete(previous);
+        if (tabIndex === 0) {
+            openTabs.delete(previous);
 
-			console.log(`Requesting to stop watching \`${previous}'...`);
+            console.log(`Requesting to stop watching \`${previous}'...`);
 
-			notifyServer.postMessage({
-				command: "remove",
-				file: previous,
-			});
-		} else {
-			tabs.splice(tabIndex, 1);
-		}
-	}
+            notifyServer.postMessage({
+                command: "remove",
+                file: previous,
+            });
+        } else {
+            tabs.splice(tabIndex, 1);
+        }
+    }
 });
 
-notifyServer.onMessage.addListener(async (event) => {
-	switch (event.type) {
-		case "update": {
-			console.log(`Received update event for \`${event.file}'...`);
+notifyServer.onMessage.addListener(async event => {
+    switch (event.type) {
+        case "update": {
+            console.log(`Received update event for \`${event.file}'...`);
 
-			const tabs = openTabs.get(event.file);
+            const tabs = openTabs.get(event.file);
 
-			await Promise.all(
-				tabs.map((tab) =>
-					browser.tabs.get(tab).then((info) => {
-						// Discarded tabs will be reloaded when they're activated
-						if (info.discarded) {
-							return Promise.resolve();
-						} else {
-							return browser.tabs.reload(tab);
-						}
-					}),
-				),
-			);
+            await Promise.all(
+                tabs.map(tab =>
+                    browser.tabs.get(tab).then(info => {
+                        // Discarded tabs will be reloaded when they're activated
+                        if (info.discarded) {
+                            return Promise.resolve();
+                        } else {
+                            return browser.tabs.reload(tab);
+                        }
+                    }),
+                ),
+            );
 
-			break;
-		}
-		case "error": {
-			console.error(`Couldn't watch \`${event.file}'!`);
+            break;
+        }
+        case "error": {
+            console.error(`Couldn't watch \`${event.file}'!`);
 
-			const tabs = openTabs.get(event.file);
+            const tabs = openTabs.get(event.file);
 
-			// Roll back changes, since the file couldn't be watched
-			for (const tab of tabs) {
-				previousOpenFile.delete(tab);
-			}
+            // Roll back changes, since the file couldn't be watched
+            for (const tab of tabs) {
+                previousOpenFile.delete(tab);
+            }
 
-			openTabs.delete(event.file);
+            openTabs.delete(event.file);
 
-			break;
-		}
-	}
+            break;
+        }
+    }
 });
 
 // XXX
